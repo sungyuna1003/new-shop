02b88f52f3009a33f33719abccff83dd89075a5e
sungyuna1003 <sungyuna1003@gmail.com>
2021-12-10 21:34:28 +0900

    createStore

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,22 +6,25 @@ function Cart(props) {
   return (
     <div>
       <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>상품명</th>
-            <th>수량</th>
-            <th>변경</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>{props.state[0].name}</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-        </tbody>
+        <tr>
+          <th>#</th>
+          <th>상품명</th>
+          <th>수량</th>
+          <th>변경</th>
+        </tr>
+
+        {props.state.map((a, i) => {
+          return (
+            <tr key={i}>
+              <td>{a.id}</td>
+              <td>{a.name}</td>
+              <td>{a.quan}</td>
+              <td>
+                <button onClick={() => {}}>+</button>
+              </td>
+            </tr>
+          );
+        })}
       </Table>
     </div>
   );
